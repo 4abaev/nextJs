4f4311e9e27b1d2761f591ce048b316d361bb66a
4abaev <progchab@gmail.com>
2023-04-11 08:52:31 +0300

    Объявлены первичные переменные в globals.css, созданы первые два компонента

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,11 @@ class MyDocument extends Document {
       <Html lang="ru">
         <Head>
             
-
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
           <body>
             <Main />
             <NextScript />
